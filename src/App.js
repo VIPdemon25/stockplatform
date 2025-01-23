143bfe6d48813ad3c4de5f533f756b5cf8c0a46d
VIPdemon25 <mrarkadas02@gmail.com>
2025-01-23 22:37:26 +0530

    chore: update package-lock.json and package.json; add new dependencies and improve styles in App.css and LandingPage.js and home page added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import LandingPage from "./components/LandingPage"
 import Login from "./components/Login"
 import SignUp from "./components/SignUp"
-import "bootstrap/dist/css/bootstrap.min.css"
+import HomePage from "./components/HomePage"
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Bootstrap JavaScript
 import "./App.css"
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
       </div>
     </Router>
