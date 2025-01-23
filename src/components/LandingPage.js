5f8d0f16ae388f7191142f23ec11aa34b4bfcbf7
VIPdemon25 <mrarkadas02@gmail.com>
2025-01-23 12:48:47 +0530

    works

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const LandingPage = () => {
   const canvasRef = useRef(null)
-
+//sdd
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext("2d")
